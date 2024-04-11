32a1cfb627cd0ff587020311f987c74845a5cad0
tythetiggers <tgoodwingaming@gmail.com>
2024-04-11 03:57:04 -0500

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SCADS")]
+[assembly: AssemblyTitle("SONIC Computer-Aided Dispatching System")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SCADS")]
+[assembly: AssemblyProduct("SONIC Computer-Aided Dispatching System")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e0684221-7562-4c13-8b61-c741ec630e70")]
+[assembly: Guid("0f8c7090-f1f1-4f6d-ba75-0d34f90e343d")]
 
 // Version information for an assembly consists of the following four values:
 //
